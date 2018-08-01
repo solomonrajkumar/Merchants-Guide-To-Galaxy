58dbe5b124d3ea74cd5e4109add81df127fe8ce7
Solomon Rajkumar <solomonrajkumar.as@gmail.com>
2018-08-01 10:58:09 +0530

    Fixed the mistake in Roman Numerals Validator spec file

--- spec/romanNumeralsValidator.spec.ts
+++ spec/romanNumeralsValidator.spec.ts
@@ -18,7 +18,6 @@ let expectedOutput5 = true;
 
 describe("Roman Numerals Validator", () => {
     describe("handle validation of Roman Numerals passed", () => {
-        //let romanNumeralsValidator = new RomanNumeralsValidator();
 
         it("should return true when valid roman numeral is passed", () => {
             let testOutput1 = RomanNumeralsValidator.isValidRomanNumber(testInput1);
@@ -26,23 +25,23 @@ describe("Roman Numerals Validator", () => {
         });
 
         it("should return true when valid roman numeral is passed", () => {
-            let testOutput1 = RomanNumeralsValidator.isValidRomanNumber(testInput1);
-            expect(testOutput1).to.equal(expectedOutput1);
+            let testOutput1 = RomanNumeralsValidator.isValidRomanNumber(testInput2);
+            expect(testOutput1).to.equal(expectedOutput2);
         });
 
         it("should return false when invalid roman numeral is passed", () => {
-            let testOutput1 = RomanNumeralsValidator.isValidRomanNumber(testInput1);
-            expect(testOutput1).to.equal(expectedOutput1);
+            let testOutput1 = RomanNumeralsValidator.isValidRomanNumber(testInput3);
+            expect(testOutput1).to.equal(expectedOutput3);
         });
 
         it("should return false when invalid roman numeral is passed", () => {
-            let testOutput1 = RomanNumeralsValidator.isValidRomanNumber(testInput1);
-            expect(testOutput1).to.equal(expectedOutput1);
+            let testOutput1 = RomanNumeralsValidator.isValidRomanNumber(testInput4);
+            expect(testOutput1).to.equal(expectedOutput4);
         });
 
         it("should return true when valid roman numeral is passed", () => {
-            let testOutput1 = RomanNumeralsValidator.isValidRomanNumber(testInput1);
-            expect(testOutput1).to.equal(expectedOutput1);
+            let testOutput1 = RomanNumeralsValidator.isValidRomanNumber(testInput5);
+            expect(testOutput1).to.equal(expectedOutput5);
         });
 
     })
